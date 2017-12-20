f0c4ac286fe2b688ffbccdb706d7f78bcb2e70c5
cwj57740 <chenweiji57740@gmail.com>
2017-12-20 23:29:59 +0800

    update

--- src/main/java/cn/edu/hit/weibo/controller/WeiboController.java
+++ src/main/java/cn/edu/hit/weibo/controller/WeiboController.java
@@ -11,7 +11,6 @@ import cn.edu.hit.weibo.util.PageUtil;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.List;
 
